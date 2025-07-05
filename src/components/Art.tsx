e5d9dfbe35e1be9e310fe74d99f66d4bebf6fa4f
Hamed <ostovari.eng@gmail.com>
2025-07-05 11:07:09 +0200

    improved art animtation

--- src/components/Art.tsx
+++ src/components/Art.tsx
@@ -7,7 +7,8 @@ const Art = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
   useGSAP(() => {
     const start = isMobile ? "top 20%" : "top top";
-    const tl = gsap.timeline({
+
+    const maskTimeline = gsap.timeline({
       scrollTrigger: {
         trigger: "#art",
         start,
@@ -16,17 +17,17 @@ const Art = () => {
         pin: true,
       },
     });
-    tl.to(".will-fade", {
-      opacity: 0,
-      ease: "power1.inOut",
-      stagger: 0.2,
-    });
-    tl.to(".masked-img", {
-      maskSize: "400%",
-      maskPosition: "center",
-      scale: 1.3,
-      ease: "power1.inOut",
-    }).to("#masked-content", { opacity: 1, duration: 1, ease: "power1.inOut" });
+
+    maskTimeline
+      .to(".will-fade", { opacity: 0, stagger: 0.2, ease: "power1.inOut" })
+      .to(".masked-img", {
+        scale: 1.3,
+        maskPosition: "center",
+        maskSize: "400%",
+        duration: 1,
+        ease: "power1.inOut ",
+      })
+      .to("#masked-content", { opacity: 1, duration: 1, ease: "power1.inOut" });
   }, []);
   return (
     <div id="art">
